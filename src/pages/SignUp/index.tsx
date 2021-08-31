08e11bf0c68d2d7526542138161c5242d9d71d98
Eliel <70238970+griffan113@users.noreply.github.com>
2021-08-31 13:05:04 -0300

    chore: add unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,36 +1,43 @@
 import React from 'react';
-import { FiArrowLeft, FiLogIn, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logo from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logo} alt="GoBarber" />
-      <form action="">
-        <h1>Faça seu cadastro</h1>
-        <Input icon={FiUser} name="user" placeholder="Nome" />
-        <Input icon={FiMail} name="email" type="email" placeholder="E-mail" />
-        <Input
-          icon={FiLock}
-          name="password"
-          type="password"
-          placeholder="Senha"
-        />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data);
+  }
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
-      <a href="login">
-        {' '}
-        <FiArrowLeft />
-        Voltar para o Log In
-      </a>
-    </Content>
-  </Container>
-);
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="GoBarber" />
+        <Form onSubmit={handleSubmit} action="">
+          <h1>Faça seu cadastro</h1>
+          <Input icon={FiUser} name="user" placeholder="Nome" />
+          <Input icon={FiMail} name="email" type="email" placeholder="E-mail" />
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+        <a href="login">
+          {' '}
+          <FiArrowLeft />
+          Voltar para o Log In
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
