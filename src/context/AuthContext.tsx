0be3ed38feadfaa2118b9297fa503627690f9e7b
Eliel <70238970+griffan113@users.noreply.github.com>
2021-09-01 09:43:30 -0300

    chore: setup hook inside AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,33 +1,54 @@
 import React, { createContext } from 'react';
+import { useState } from 'react';
 import { useCallback } from 'react';
 
 import { api } from '../services/api';
 
+interface AuthState {
+  token: string;
+  user: object;
+}
 interface ISignInCreadentials {
   email: string;
   password: string;
 }
 
 interface AuthContextState {
-  name: string;
+  user: object;
   signIn(data: ISignInCreadentials): Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextState>({} as AuthContextState);
 
 const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(
     async ({ email, password }: ISignInCreadentials) => {
       const response = await api.post('sessions', { email, password });
 
-      console.log(response);
+      const { token, user } = response.data;
+
+      localStorage.setItem('@GoBarber:token', token);
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+      setData({ token, user });
     },
     [],
   );
 
   return (
     /* @ts-ignore */
-    <AuthContext.Provider value={{ name: 'Eliel', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
